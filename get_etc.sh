7a210bebf5dcc8be068819f7a368c03ed87e722c
Bryan M.  Karpowicz <bkarpowi@gs6101-rocket.ndc.nasa.gov>
2018-11-20 16:48:32 -0500

    add some mkdirs to make plots directory and etc directory

--- get_etc.sh
+++ get_etc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir etc
+mkdir plots
 wget -O etc/rtcoef_metop_2_iasi_so2.H5 https://www.nwpsaf.eu/downloads/rtcoef_rttov12/rttov9pred101L/rtcoef_metop_2_iasi_so2.H5
 wget -O etc/rtcoef_eos_2_airs_so2.H5 https://www.nwpsaf.eu/downloads/rtcoef_rttov12/rttov9pred101L/rtcoef_eos_2_airs_so2.H5
 wget -O etc/rtcoef_jpss_0_cris_so2.H5 https://www.nwpsaf.eu/downloads/rtcoef_rttov12/rttov9pred101L/rtcoef_jpss_0_cris_so2.H5
